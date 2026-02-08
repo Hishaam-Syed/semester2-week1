a32a3bda172bc1188c595d013f2705ff016330e5
Hishaam-Syed <lprm0785@leeds.ac.uk>
2026-02-08 04:44:55 +0000

    done

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -6,4 +6,11 @@
 -- 1. Open SQLite in terminal: sqlite3 countries.db
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
-
+-- SELECT Continent, SUM(Population) FROM countries;
+-- SELECT Continent, SUM(Population) FROM countries GROUP BY Continent;
+-- SELECT Continent, COUNT(*) FROM countries GROUP BY Continent;
+-- SELECT Continent, COUNT(*) AS TotalCountries FROM countries GROUP BY Continent;
+-- SELECT Continent, AVG(Population) FROM countries GROUP BY Continent HAVING AVG(Population) > 1000000;
+-- Select Continent, AVG(Population) FROm countries GROUP BY Continent;
+-- SELECT Continent, SUM(Population) FROM countries GROUP By Continent;
+-- SELECT Country ,Continent,  Population from countries GROUP BY Continent ORDER BY Population DESC; 
--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,4 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+SELECT DepartmentName, count(EnrolmentID) from Department JOIN Course ON Department.Departmentid=course.Departmentid Join Enrolment ON course.courseid=enrolment.courseid GROUP BY DepartmentName;
